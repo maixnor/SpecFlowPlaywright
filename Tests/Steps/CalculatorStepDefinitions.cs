6801bf54d29cda228e7fc5b05310b8acd582eb35
Benjamin Meixner <mei18930@spengergasse.at>
2021-12-06 10:04:51 +0100

    Calculator

--- Tests/Steps/CalculatorStepDefinitions.cs
+++ Tests/Steps/CalculatorStepDefinitions.cs
@@ -1,57 +1,47 @@
-﻿using TechTalk.SpecFlow;
+﻿using FluentAssertions;
+using Source;
+using TechTalk.SpecFlow;
 
 namespace SpecFlow.Steps
 {
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
-        // For additional details on Tests step definitions see https://go.specflow.org/doc-stepdef
-
+        private readonly Calculator _calculator;
         private readonly ScenarioContext _scenarioContext;
+
+        private int firstNumber;
+        private int secondNumber;
+        private int result;
 
         public CalculatorStepDefinitions(ScenarioContext scenarioContext)
         {
             _scenarioContext = scenarioContext;
+            _calculator = new Calculator();
         }
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            _scenarioContext.Pending();
+            firstNumber = number;
         }
 
         [Given("the second number is (.*)")]
         public void GivenTheSecondNumberIs(int number)
         {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
-
-            _scenarioContext.Pending();
+            secondNumber = number;
         }
 
         [When("the two numbers are added")]
         public void WhenTheTwoNumbersAreAdded()
         {
-            //TODO: implement act (action) logic
-
-            _scenarioContext.Pending();
+            result = _calculator.Add(firstNumber, secondNumber);
         }
 
         [Then("the result should be (.*)")]
         public void ThenTheResultShouldBe(int result)
         {
-            //TODO: implement assert (verification) logic
-
-            _scenarioContext.Pending();
+            this.result.Should().Be(result);
         }
     }
 }
